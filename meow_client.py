6df0beac54615667f975d0b4eb2834297881618b
Nic Desjardins <nic@nicdesjardins.com>
2021-12-02 09:48:36 -0500

    MeoClient concept is working so far, need to write the beginning of the server, which I may want to have be similar in structure or just keep it in one piece, I'll see...

--- meow_client.py
+++ meow_client.py
@@ -1,5 +1,21 @@
 from meow import Meow
 from config import GetUserDetails, ConfirmAnswers
+from network_client import NetworkClient
+from parcel import Parcel
+
+'''
+MeowClient:
+- Inherits Meow
+- Gets config from config.py (config.py inherits from Meow)
+
+To do:
+- Connect to a server w/ network_client (will it inherit Meow?)
+- Convert its object(s) into data using Parcel
+- Send data converted by Parcel to the server via the network_client
+- network client will receive data
+- data will be converted back to a usable object by Parcel
+- MeowClient will then action (e.g. display w/ ui, do an action, etc)
+'''
 
 class MeowClient(Meow):
 
@@ -10,10 +26,17 @@ class MeowClient(Meow):
                 ConfirmAnswers()
             
             print("\nOK, then we're all set to keep going!\n")
+            self.netClient = NetworkClient(self.settings.server, self.settings.port, self.netHandler)
+            self.netClient.connect()
 
         except KeyboardInterrupt as ex:
             print('\nOk then, bye!')
             pass
+    
+    def netHandler(self, data):
+        p = Parcel()
+        p.unpack(data)
+        print("Received " +str(p))
 
 class ConnectToServer(Meow):
     def __init__(self):
